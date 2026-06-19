4f6ab10c1f72c3cf282350eab9e2ae2f22a65c6e
ismailgito <dadobeb553@gmail.com>
2026-06-19 17:23:29 +0530

    New GTM Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import '../styles/bootstrap.min.css'; 
 import '../styles/style.css';
+import GTM from '@/components/GTM'; 
 
 export const metadata = {
   title: 'Mohamed Ismail - Performance Marketer Portfolio',
@@ -7,6 +8,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  // Can be configured via NEXT_PUBLIC_GTM_ID in .env
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID  
+
   return (
     <html lang="en">
       <head>
@@ -20,6 +24,9 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
       </head>
       <body>
+        {/* GTM placed right at the top of the body for accurate tracking */}
+        <GTM gtmId={GTM_ID} />
+
         {children}
       </body>
     </html>
